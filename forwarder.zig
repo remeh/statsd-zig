20ce50d1633e93828bf580e819f7e19f4bb9b485
remeh <remeh@remeh.fr>
2020-05-11 23:03:43 +0200

    forwarder: WIP curl HTTP post call.

--- forwarder.zig
+++ forwarder.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const c = @cImport(@cInclude("curl/curl.h"));
 
 const Sample = @import("sampler.zig").Sample;
 const metric = @import("metric.zig");
@@ -7,12 +8,12 @@ pub const Forwarder = struct {
     /// flush is responsible for sending all the given metrics to some HTTP route.
     /// It owns the list of metrics and is responsible for freeing its memory.
     pub fn flush(allocator: *std.mem.Allocator, samples: std.AutoHashMap(u64, Sample)) !void {
-        var response = try std.ArrayListSentineled(u8, 0).initSize(allocator, 0);
-        defer response.deinit();
+        var buf = try std.ArrayListSentineled(u8, 0).initSize(allocator, 0);
+        defer buf.deinit();
         defer samples.deinit();
 
-        try response.resize(11);
-        try response.appendSlice("{\"series\":[");
+        try buf.resize(11);
+        try buf.appendSlice("{\"series\":[");
 
         // append every sample
 
@@ -21,17 +22,19 @@ pub const Forwarder = struct {
         var kv = iterator.next();
         while (kv != null) {
             if (!first) {
-                try response.resize(response.len() + 1);
-                try response.append(',');
+                try buf.resize(buf.len() + 1);
+                try buf.append(',');
             }
             first = false;
-            try write_sample(allocator, &response, kv.?.value);
+            try write_sample(allocator, &buf, kv.?.value);
             kv = iterator.next();
         }
 
-        try response.resize(response.len() + 2);
-        try response.appendSlice("]}");
-        std.debug.warn("{}", .{response.span()});
+        try buf.resize(buf.len() + 2);
+        try buf.appendSlice("]}");
+
+        send_http_request(&buf);
+        std.debug.warn("{}", .{buf.span()});
     }
 
     fn write_sample(allocator: *std.mem.Allocator, buf: *std.ArrayListSentineled(u8, 0), sample: Sample) !void {
@@ -77,5 +80,32 @@ pub const Forwarder = struct {
 
         // append it to the main buffer
         try buf.*.appendSlice(json);
+    }
+
+    fn send_http_request(buf: *std.ArrayListSentineled(u8, 0)) void {
+        _ = c.curl_global_init(c.CURL_GLOBAL_ALL);
+
+        var curl: ?*c.CURL = undefined;
+        var res: c.CURLcode = undefined;
+        var headers: [*c]c.curl_slist = null;
+
+        curl = c.curl_easy_init();
+        if (curl != null) {
+            _ = c.curl_easy_setopt(curl, @intToEnum(c.CURLoption, c.CURLOPT_URL), "http://localhost:8080/?apikey=hello");
+            _ = c.curl_easy_setopt(curl, @intToEnum(c.CURLoption, c.CURLOPT_POSTFIELDS), @ptrCast([*c]u8, buf.*.span()));
+            _ = c.curl_slist_append(headers, "Content-Type: application/json");
+            _ = c.curl_slist_append(headers, "Dd-Api-Key: hello");
+            _ = c.curl_easy_setopt(curl, @intToEnum(c.CURLoption, c.CURLOPT_HTTPHEADER), headers);
+
+            res = c.curl_easy_perform(curl);
+            if (@enumToInt(res) != @bitCast(c_uint, c.CURLE_OK)) {
+                _ = c.fprintf(c.stderr, "curl_easy_perform() failed: %s\n", c.curl_easy_strerror(res));
+            }
+
+            c.curl_easy_cleanup(curl);
+        }
+
+        c.curl_global_cleanup();
+        std.debug.warn("http call done", .{});
     }
 };
