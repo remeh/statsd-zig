464dfc265ba654e35aa3c9a720f48e359d36faa8
remeh <remeh@remeh.fr>
2020-11-04 13:13:55 +0100

    measure_allocator: add a unit test.

--- src/measure_allocator.zig
+++ src/measure_allocator.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const warn = std.debug.warn;
+const assert = @import("std").debug.assert;
 
 /// MeasureAllocator is counting how much bytes were succesfully allocated with
 /// the parent_allocator.
@@ -47,4 +49,32 @@ pub fn MeasureAllocator() type {
             return result;
         }
     };
+}
+
+test "measure allocator counter and memory leak" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    var measure_allocator = MeasureAllocator().init(&arena.allocator);
+
+    var loop_count: u32 = 0;
+    var alloc_count: u32 = 0;
+    var reset_count: u32 = 0;
+    while (loop_count < 1024) {
+        var data = try measure_allocator.allocator.alloc(u8, 8192);
+        measure_allocator.allocator.free(data); // is actually not freeing anything
+        assert(measure_allocator.allocated == (8192 * (alloc_count + 1)));
+        alloc_count += 1;
+        loop_count += 1;
+
+        if (measure_allocator.allocated > 15000) {
+            arena.deinit();
+            arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+            measure_allocator = MeasureAllocator().init(&arena.allocator);
+            reset_count += 1;
+            alloc_count = 0;
+        }
+    }
+
+    assert(reset_count == 1024 / 2); // it should reset every 2 runs
+
+    arena.deinit(); // is freeing all the data
 }
